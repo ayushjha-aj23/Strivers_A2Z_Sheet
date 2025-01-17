9a509f174f3b62cd29efff9fbcf2ffd7ed11f750
Ayush Jha <aj23.workspace@gmail.com>
2025-01-17 23:56:55 +0530

    Create 05_SearchInLL.java

---
+++ LinkedList/SinglyLL/Easy/05_SearchInLL.java
@@ -0,0 +1,23 @@
+/* Node of a linked list
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        // Code here
+        
+        Node temp = head;
+        
+        while(temp != null){
+            if(temp.data == key)
+                return true;
+            else
+                temp = temp.next;
+        }
+        
+        return false;
+    }
+}
