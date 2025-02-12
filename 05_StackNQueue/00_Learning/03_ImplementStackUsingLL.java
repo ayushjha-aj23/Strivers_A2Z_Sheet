1674381297bfdf5a6a44a7710ae45174756fa527
Ayush Jha <aj23.workspace@gmail.com>
2025-02-12 13:00:16 +0530

    Create 03_ImplementStackUsingLL.java

---
+++ 05_StackNQueue/00_Learning/03_ImplementStackUsingLL.java
@@ -0,0 +1,55 @@
+class MyStack {
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    // 
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }
+    
+    
+    StackNode top; // Right now pointing to null
+    int size = 0;
+    
+    // TC and SC is O(1) and here as it is dynamic so the SC is much less than in case of Array Implementation
+
+    // Function to push an integer into the stack.
+    void push(int a) {
+        // Add your code here
+        
+        // We will not check overflow here becuase here stack is dynamic in nature
+        
+        StackNode newNode = new StackNode(a); // Create new Node
+        newNode.next = top; // NewNode pointing to top
+        top = newNode; // Update top as newNode
+        size++; // Increase the size
+    }
+
+    // Function to remove an item from top of the stack.
+    int pop() {
+        // Add your code here
+        
+        // Check if the stack is empty or not - UNDERFLOW
+        if(top==null) return -1;
+        
+        StackNode temp = top; // Store top in temp 
+        // and move top to next and temp will automatically be discarded by Garbage Collector 
+        top = top.next; // Move top to point to next node
+        size--; // Decrease the size
+        
+        return temp.data; // Return the data of poped element
+    }
+    
+    int top(){
+        // Check if the stack is empty or not - UNDERFLOW
+        if(top==null) return -1;
+        return top.data;
+    }
+    
+    int sizeofStack(){
+        return size;
+    }
+    
+}
