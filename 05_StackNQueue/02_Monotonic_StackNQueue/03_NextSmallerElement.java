9197168cfcb6a224be1013021d53f363ed633bda
Ayush Jha <aj23.workspace@gmail.com>
2025-02-21 16:49:37 +0530

    Update 03_NextSmallerElement.java

--- 05_StackNQueue/02_Monotonic_StackNQueue/03_NextSmallerElement.java
+++ 05_StackNQueue/02_Monotonic_StackNQueue/03_NextSmallerElement.java
@@ -1,1 +1,20 @@
+class Solution {
 
+  public int[] prevSmaller(int[] A) {
+      int[] result = new int[A.length];
+      Stack<Integer> stack = new Stack<>();
+      
+      for (int i = 0; i < A.length; i++) {
+          while (!stack.isEmpty() && stack.peek() >= A[i]) {
+              stack.pop();
+          }
+          if (stack.isEmpty()) {
+              result[i] = -1;
+          } else {
+              result[i] = stack.peek();
+          }
+          stack.push(A[i]);
+      }
+      return result;
+  }
+}
