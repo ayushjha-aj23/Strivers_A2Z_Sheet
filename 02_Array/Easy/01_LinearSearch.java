69c99329b9e8f15a58bcee30a4a5d2806408c98e
Ayush Jha <aj23.workspace@gmail.com>
2025-02-24 17:10:09 +0530

    Create 01_LinearSearch.java

---
+++ 02_Array/Easy/01_LinearSearch.java
@@ -0,0 +1,16 @@
+// Linear Search in an Array
+
+class Solution {
+
+    // Linear Search 
+    // TC - O(N) and SC - O(1)
+    static int search(int arr[], int x) {
+
+        // Your code here
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==x)
+                return i;
+        }
+        return -1;
+    }
+}
