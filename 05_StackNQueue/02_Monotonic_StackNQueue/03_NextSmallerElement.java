9041d87bfd9e61edb72214346729b0491b449104
Ayush Jha <aj23.workspace@gmail.com>
2025-02-25 12:09:04 +0530

    Update 03_NextSmallerElement.java

--- 05_StackNQueue/02_Monotonic_StackNQueue/03_NextSmallerElement.java
+++ 05_StackNQueue/02_Monotonic_StackNQueue/03_NextSmallerElement.java
@@ -1,19 +1,28 @@
 class Solution {
 
   public int[] prevSmaller(int[] A) {
+    
       int[] result = new int[A.length];
-      Stack<Integer> stack = new Stack<>();
+      Stack<Integer> st = new Stack<>();
       
       for (int i = 0; i < A.length; i++) {
-          while (!stack.isEmpty() && stack.peek() >= A[i]) {
-              stack.pop();
+
+          // If top is greater than or equal to a[i] - then pop
+          while (!st.isEmpty() && st.peek() >= A[i]) {
+              st.pop();
           }
-          if (stack.isEmpty()) {
+
+          // If stack is not empty - set a[i] as NGE
+          // We have 2 ways to write - by if statement or by using ternary operator
+          // Alternatively we can also initialize the result[] with -1 at the beginning
+          if (st.isEmpty()) 
               result[i] = -1;
-          } else {
-              result[i] = stack.peek();
-          }
-          stack.push(A[i]);
+            else
+              result[i] = st.peek();
+          // OR  
+          // result[i] = !st.isEmpty()? st.peek():-1;
+          
+          st.push(A[i]);
       }
       return result;
   }
