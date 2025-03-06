325605e9c638e4ee6a3de48096e4d0372240273d
Ayush Jha <aj23.workspace@gmail.com>
2025-03-06 15:48:39 +0530

    Update 01_CountDigits.java

--- 01_Basics/03_BasicMaths/01_CountDigits.java
+++ 01_Basics/03_BasicMaths/01_CountDigits.java
@@ -4,19 +4,20 @@
 class Solution {
     static int evenlyDivides(int n) {
         // code here
-        
-        int temp = n;
+        int temp = n; // make a copy of n
         int count = 0;
         
-        
         while(temp!=0){
-            int lastDigit = temp%10;
+            int lastDigit = temp%10; // Get the last digit
+            
             // First check lastDigit should not be equal to 0
-            // Also check if n is completely divisible by lastDigit or not 
+            // Also then check if n is completely divisible by lastDigit or not 
+            // If we 1st check n%lastDigit==0 where lastDigit = 0 then we will get ArithmeticException
+            
             if(lastDigit!=0 && n%lastDigit==0)
-                count++;
-            temp = temp/10;
+                count++; // Increment Count
+            temp = temp/10; // Remove the last Digit
         }
-        return count;
+        return count; 
     }
 }
