3623d2c8cd1b22c99107fdfdb2bb401343a54132
Ayush Jha <aj23.workspace@gmail.com>
2025-03-07 12:09:24 +0530

    Create 03_PallindromeNumber.java

---
+++ 01_Basics/03_BasicMaths/03_PallindromeNumber.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        // Case 1: If x is -ve because -151 is not equal to 151-
+        if(x<0)
+            return false;
+
+        // Case 2: If x is +ve
+        int temp = x;
+        int rev = 0;
+
+        while(temp!=0){
+            int lastDigit = temp%10;
+            rev = (rev*10)+lastDigit;
+            temp = temp/10;
+        }
+
+        if(rev==x)
+            return true;
+        else
+            return false;
+    }
+}
