1ff4870d0575be6565555debd3bb158a2735439c
Ayush Jha <aj23.workspace@gmail.com>
2025-03-08 21:41:35 +0530

    Update 07_CheckForPrime.java

--- 01_Basics/03_BasicMaths/07_CheckForPrime.java
+++ 01_Basics/03_BasicMaths/07_CheckForPrime.java
@@ -1,1 +1,37 @@
-
+class Solution {
+    
+    // Naive Approach - TC-O(N) and SC-O()
+    /*
+    static boolean isPrime(int n) {
+        // code here
+        
+        for(int i=2; i<n; i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    */
+    
+    
+    // Optimised Approach - 
+    static boolean isPrime(int n) {
+        // code here
+        
+        if(n<=1)
+            return false;
+        if(n==2) // Only Prime number which is even 
+            return true;
+        if(n%2==0) // All other even numbers are prime
+            return false;
+        
+        
+        // Start from 3 and every time skip even number so i+=2
+        // And till i*i<=n, equal case is to check for perfect square 
+        for(int i=3; i*i<=n; i+=2){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+}
