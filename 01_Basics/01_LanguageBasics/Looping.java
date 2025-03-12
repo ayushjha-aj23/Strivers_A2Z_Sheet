7c55c798f4942e196431101933725c8c1de704be
Ayush Jha <aj23.workspace@gmail.com>
2025-03-12 16:40:59 +0530

    Create Looping.java

---
+++ 01_Basics/01_LanguageBasics/Looping.java
@@ -0,0 +1,17 @@
+//Back-end complete function Template for Java
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        // Take the input from the user using Scanner and store in n
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int i=10;
+        while(i>0){
+            System.out.print(n*i + " ");
+            i--;
+        }
+    }
+}
