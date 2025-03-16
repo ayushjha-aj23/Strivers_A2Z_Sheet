96a888550a7bdafcbe0518876056827de7229481
Ayush Jha <aj23.workspace@gmail.com>
2025-03-16 18:03:27 +0530

    Update 03_SumOf1stN_Numbers.java

--- 01_Basics/04_BasicRecursion.java/03_SumOf1stN_Numbers.java
+++ 01_Basics/04_BasicRecursion.java/03_SumOf1stN_Numbers.java
@@ -1,1 +1,21 @@
 
+// User function Template for Java
+class Solution {
+    int sumOfSeries(int n) {
+        // code here
+        
+        return sumOfCubesTillN(n);
+    }
+    
+    
+    int sumOfCubesTillN(int N){
+        
+        int cubesSum = 0;
+        // Base Case
+        if(N==0)
+            return cubesSum;
+         
+        cubesSum = N*N*N + sumOfCubesTillN(N-1);
+        return cubesSum;
+    }
+}
