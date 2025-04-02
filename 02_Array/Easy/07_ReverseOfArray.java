d16fa15f7b4b95aa35aa328e7bedc4273f80ae8f
Ayush Jha <aj23.workspace@gmail.com>
2025-04-02 13:34:10 +0530

    Update 07_ReverseOfArray.java

--- 02_Array/Easy/07_ReverseOfArray.java
+++ 02_Array/Easy/07_ReverseOfArray.java
@@ -31,5 +31,32 @@ class Solution {
             right--;
         }
     }
+
+    // Using recursion and 2 pointer left and right - (a, 0, a.length-1)
+    // We can also do it with 1 pointer - (a, i, a.length-1-i)
+    /*
+    public static void main(String[] args) {
+	
+		int a[] = {1, 2, 4, 6, 9};
+		
+		reverseArray(a,0,a.length-1);
+		
+		for(int i=0; i<a.length; i++){
+		    System.out.print(a[i] + " ");
+		}
+	}
+	
+	public static void reverseArray(int[] a, int l, int r){
+	    
+	    if(l>=r)
+	        return;
+	   
+	    int temp = a[l];
+	    a[l] = a[r];
+	    a[r] = temp;
+	    
+	    reverseArray(a, l+1, r-1);
+	}
+    */
     
 }
