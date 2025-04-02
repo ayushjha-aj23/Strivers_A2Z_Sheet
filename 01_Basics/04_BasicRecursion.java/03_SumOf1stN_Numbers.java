b55b9b900a647d9fb644feed9d00ded0e401ea6f
Ayush Jha <aj23.workspace@gmail.com>
2025-04-02 13:30:27 +0530

    Update 03_SumOf1stN_Numbers.java

--- 01_Basics/04_BasicRecursion.java/03_SumOf1stN_Numbers.java
+++ 01_Basics/04_BasicRecursion.java/03_SumOf1stN_Numbers.java
@@ -1,15 +1,37 @@
+// Find the sum of 1st n terms
+public class Main
+{
+	public static void main(String[] args) {
+        recursionSum(3, 0); // sum till given num and initial sum as 0
+	}
+	
+	public static void recursionSum(int i, int sum){
+	   // Base Case:
+	   if(i<1){
+	    System.out.println(sum);
+	    return;
+	   }
+	   
+	   // Call Itself
+	   recursionSum(i-1, sum+i);
+	}
+}
+
+
+
+
+// Find the sum of cubes of 1st n terms
 
 // User function Template for Java
+/*
 class Solution {
     int sumOfSeries(int n) {
         // code here
-        
         return sumOfCubesTillN(n);
     }
     
     
     int sumOfCubesTillN(int N){
-        
         int cubesSum = 0;
         // Base Case
         if(N==0)
@@ -19,3 +41,4 @@ class Solution {
         return cubesSum;
     }
 }
+*/
