be5c37cf6ca34e4142297d3e5f45d70955d77c7a
Ayush Jha <aj23.workspace@gmail.com>
2025-06-02 15:36:44 +0530

    Create 03_Pattern.java

---
+++ 01_Basics/02_Patterns[Loops]/03_Pattern.java
@@ -0,0 +1,22 @@
+
+
+/*
+1
+12
+123
+1234
+12345
+// Same as 2 just replace '*' with j in the Sopln
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+	}
+}
