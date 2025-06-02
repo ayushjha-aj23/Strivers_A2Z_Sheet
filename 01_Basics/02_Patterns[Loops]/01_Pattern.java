68f3e4db27ad43b8f0a96a8fde6046c2df5b571e
Ayush Jha <aj23.workspace@gmail.com>
2025-06-02 15:26:34 +0530

    Create 01_Pattern.java

---
+++ 01_Basics/02_Patterns[Loops]/01_Pattern.java
@@ -0,0 +1,28 @@
+/*
+*****
+*****
+*****
+*****
+*****
+*/
+
+// Remeber -> Outer Loop for Lines and Inner Loop for Columns
+/*
+Rules 
+1. For the Outer Loop -> Count the number of lines
+2. For the Inner Loop -> Focus on the columns and somehow connect to the rows
+3. Print them (say *) inside the inner for loop
+4. Observe Symmetry (Optional)
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+}
