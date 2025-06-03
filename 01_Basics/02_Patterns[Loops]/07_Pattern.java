4791ec4d095efe09b16d88fa6073f25afd5b2846
Ayush Jha <aj23.workspace@gmail.com>
2025-06-03 13:29:46 +0530

    Update 07_Pattern.java

--- 01_Basics/02_Patterns[Loops]/07_Pattern.java
+++ 01_Basics/02_Patterns[Loops]/07_Pattern.java
@@ -1,5 +1,3 @@
-
-
 /*
     *
    ***
@@ -10,4 +8,33 @@
 
 // Space, Star, Space
 
-// 
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int spaces_a= 4;
+	    int spaces_b= 4;
+	    int stars = 1;
+	    
+        for(int i=0; i<5; i++){
+            // Space
+            for(int j=1; j<=spaces_a; j++){
+                System.out.print("a");
+            }
+            spaces_a--;
+            
+            // Star
+            for(int j=1; j<=stars;j++){
+                System.out.print("*");
+            }
+            stars+=2;
+                
+            // Spaces 
+            for(int j=1; j<=spaces_b; j++){
+                System.out.print("b");
+            }
+            spaces_b--;
+            System.out.println();
+        }
+	}
+}
