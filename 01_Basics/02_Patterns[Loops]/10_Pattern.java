8ef1b8e152850fb09cd04e77b09a3a5a86015f26
Ayush Jha <aj23.workspace@gmail.com>
2025-06-04 11:25:53 +0530

    Update 10_Pattern.java

--- 01_Basics/02_Patterns[Loops]/10_Pattern.java
+++ 01_Basics/02_Patterns[Loops]/10_Pattern.java
@@ -1,6 +1,7 @@
 
-
 /*
+Pattern - 10: Half Diamond Star Pattern
+
 *
 **
 ***
@@ -17,17 +18,17 @@
 public class Main
 {
 	public static void main(String[] args) {
-	    
-  for (int i = 1; i <= 9; i++) {
-      
-      // If i is less then or equal to 5 then i times 1->5
-      // If i is more than 5 then 10-i times 4->1  
-      int stars = i <= 5 ? i : 10 - i;// 1 to 5, then 4 to 1
-      
-      for (int j = 1; j <= stars; j++) {
-          System.out.print("*");
-      }
-      System.out.println();
-    }
+	int n=5;
+	  for (int i = 1; i <=2*n-1; i++) {
+	      
+	      // If i is less then or equal to 5 then i times 1->5
+	      // If i is more than 5 then 10-i times 4->1  
+	      int stars = i <= n ? i : 2*n - i;// 1 to 5, then 4 to 1
+	      
+	      for (int j = 1; j <= stars; j++) {
+	          System.out.print("*");
+	      }
+	      System.out.println();
+	    }
 	}
 }
