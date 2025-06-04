eea370fe52fa9983e053ae0c57c0a529339ccc55
Ayush Jha <aj23.workspace@gmail.com>
2025-06-04 10:47:09 +0530

    Create 12_Pattern.java

---
+++ 01_Basics/02_Patterns[Loops]/12_Pattern.java
@@ -0,0 +1,29 @@
+public class Main
+{
+  
+  // Number, Space, Number
+	public static void main(String[] args) {
+		
+		int n = 5;
+		// Outer Loop 
+		for(int i=1; i<=n; i++){
+		    
+		    // Number 
+		    for(int j=1; j<=i; j++){
+		        System.out.print(j);
+		    }
+		    
+		    // Spaces
+		    for(int j=1; j<=(2*n)-(2*i); j++){
+		        System.out.print(" ");
+		    }
+		    
+		    // Number 
+		    for(int j=i; j>=1; j--){
+		        System.out.print(j);
+		    }
+		    
+		    System.out.println();
+		}
+	}
+}
