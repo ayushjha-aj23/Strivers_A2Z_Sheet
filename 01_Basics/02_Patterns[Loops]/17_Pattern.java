f3887ee2dddef0ad3c5b0a9aced2bb7dc717bae0
Ayush Jha <aj23.workspace@gmail.com>
2025-06-05 11:42:08 +0530

    Create 17_Pattern.java

---
+++ 01_Basics/02_Patterns[Loops]/17_Pattern.java
@@ -0,0 +1,45 @@
+
+/*
+Pattern - 17: Alpha-Hill Pattern
+
+   A
+  ABA
+ ABCBA
+ABCDCBA
+
+Space, Character, Space
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+        
+        int n=5;
+        for(int i=0; i<n; i++){
+            
+            // Spaces 
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            
+            // Characters 
+            int breakpoint = (2*i+1)/2;
+            char c = 'A';
+            for(int j=1; j<=2*i+1;j++){
+                System.out.print(c);
+                
+                if(j<=breakpoint) 
+                    c++;
+                else 
+                    c--;
+            }
+            
+            // Spaces 
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+        }
+	}
+}
