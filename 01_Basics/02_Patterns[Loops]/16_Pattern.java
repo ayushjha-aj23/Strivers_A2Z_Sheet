ce733480e4e0988ee9e5a59f0f1e807b7b50affb
Ayush Jha <aj23.workspace@gmail.com>
2025-06-05 11:18:39 +0530

    Create 16_Pattern.java

---
+++ 01_Basics/02_Patterns[Loops]/16_Pattern.java
@@ -0,0 +1,27 @@
+
+
+/*
+A
+BB
+CCC
+DDDD
+EEEEE
+*/
+
+// char c = 'A' and everytime just increase it by 1 i.e. c++
+
+public class Main
+{
+	public static void main(String[] args) {
+        
+        int n=5;
+        char c = 'A';
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                System.out.print(c);  
+            }
+            c++;
+            System.out.println();
+        }
+	}
+}
