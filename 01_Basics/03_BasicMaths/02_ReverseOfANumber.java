2628c7b40ec5e0101532643ae2b20f79ce5e5731
Ayush Jha <aj23.workspace@gmail.com>
2025-06-13 14:12:27 +0530

    Update 02_ReverseOfANumber.java

--- 01_Basics/03_BasicMaths/02_ReverseOfANumber.java
+++ 01_Basics/03_BasicMaths/02_ReverseOfANumber.java
@@ -1,3 +1,25 @@
+// GFG - Reverse Digit 
+// User function Template for Java
+
+class Solution {
+    public int reverseDigits(int n) {
+        // Code here
+        
+        int temp = n;
+        int digitFromLast = 0;
+        int reverseDigit = 0;
+        
+        while(temp!=0){
+            digitFromLast = temp%10;
+            reverseDigit = reverseDigit*10 + digitFromLast;   
+            temp = temp/10;
+        }
+        return reverseDigit;
+        
+    }
+}
+
+// LC-7 -> Reverse Integer
 class Solution {
     public int reverse(int x) {
 
@@ -14,11 +36,13 @@ class Solution {
         while(a>0){
             int lastDigit = a%10; // Extracr the last digit
 
+            // -2,147,483,648
             // Check if result will overflow or not (1)when multiplying by 10 (2) If result*10=MAX_VALUE and lastDigit is > 7
             if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && lastDigit > 7)) {
                 return 0; // Overflow case for positive numbers
             }
 
+            // 2,147,483,647
             // Check if result will underflow or not (1)when multiplying by 10 (2) If result*10=MIN_VALUE and lastDigit<-8 like -9
             if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && lastDigit < -8)) {
                 return 0; // Overflow case for negative numbers
