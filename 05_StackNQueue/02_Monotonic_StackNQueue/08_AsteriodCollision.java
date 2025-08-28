c5be2d6ea70e5d7203b99752920de48581e003db
Ayush Jha <aj23.workspace@gmail.com>
2025-08-29 01:23:47 +0530

    Update 08_AsteriodCollision.java

--- 05_StackNQueue/02_Monotonic_StackNQueue/08_AsteriodCollision.java
+++ 05_StackNQueue/02_Monotonic_StackNQueue/08_AsteriodCollision.java
@@ -7,7 +7,7 @@ class Solution {
         
         ArrayList<Integer> list = new ArrayList<>();
         
-        // Stack<Integer> st = new Stack<>();
+        // Stack<Integer> st = new Stack<>(); // Not used as it will give opposite order in array so list is preferred
 
         // if((asteroids.length==2) && asteroids[0] == (int) Math.abs(asteroids[1]))
         //     return 
@@ -18,15 +18,17 @@ class Solution {
                 list.add(asteroids[i]);
             else{ // If negative
                 // If absolute value of top is less than current element - Pop...E.g. - [2, -5] -> remove 2
+                // list is not empty, list's top is +ve, list's top element<current element--- remove top
                 while( list.size()>0 && list.get(list.size()-1)>=0 && list.get(list.size()-1)<Math.abs(asteroids[i])){
                     list.remove(list.size()-1);
                 }
 
                 // If absolute value of top is equal to current element - Pop...E.g. [8, -8] -> remove 8
+                // list is not empty, list's top is +ve, list's top element==current element--- remove top and go ahead
                 if(list.size()>0 && list.get(list.size()-1)>=0 && list.get(list.size()-1)==Math.abs(asteroids[i])){
                     list.remove(list.size()-1);
                 }else if(list.size()==0 || list.get(list.size()-1)<0){ // if list is empty or the current is negative
-                    list.add(asteroids[i]);
+                    list.add(asteroids[i]); // Add the negative element in the list
                 }
             }  
         }
