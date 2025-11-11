fd6656053b166624cbdb4faf0f181165682f52a5
Ayush Jha <aj23.workspace@gmail.com>
2025-11-12 01:53:08 +0530

    Create 03_SearchInsertPosition.java

---
+++ 01_Basics/06_BinarySearch/03_SearchInsertPosition.java
@@ -0,0 +1,26 @@
+// LC - 35
+
+class Solution {
+
+    // Same Code as Lower Bound 
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int result = n;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>=target){
+                result = mid;
+                high = mid-1;
+            }else
+                low = mid+1;
+        }
+
+        return result;
+    }
+}
