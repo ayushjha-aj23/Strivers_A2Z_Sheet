4cbf87ca987687cadff118c9efeb073198794722
Ayush Jha <aj23.workspace@gmail.com>
2025-11-12 02:05:17 +0530

    Create 04_FloorNCeil.java

---
+++ 01_Basics/06_BinarySearch/04_FloorNCeil.java
@@ -0,0 +1,6 @@
+
+// Finding the Floor and Ceil Value in a Sorted Array
+
+// Floor - Largest Value which is <=x (Target Value)
+// Ceil - Smallest Value which is >=x (Target Value) ---> lower bound
+
