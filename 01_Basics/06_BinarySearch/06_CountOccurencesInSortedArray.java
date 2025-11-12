79ca23d5f1e6f339a5dd1412ebf1c41f3c3a4191
Ayush Jha <aj23.workspace@gmail.com>
2025-11-12 12:45:53 +0530

    Add binary search method to count occurrences
    Implemented countFreq method using binary search to count occurrences of a target in a sorted array.

--- 01_Basics/06_BinarySearch/06_CountOccurencesInSortedArray.java
+++ 01_Basics/06_BinarySearch/06_CountOccurencesInSortedArray.java
@@ -1,3 +1,79 @@
 
 // GFG
+class Solution {
+    
+    // Linear Search
+    // TC - O(N)
+    /*
+    int countFreq(int[] arr, int target) {
+        // code here
+        
+        int n = arr.length;
+        int count =0;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i]==target)
+                count++;
+        }
+        
+        return count;
+    }
+    */
+    
+    // Binary Search
+    int countFreq(int[] arr, int target) {
+        // code here
+        
+        // arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2 -> Output = 4
+        // firstOccurence(LB) = 2 n lastOccurence(UB-1) = 5 
+        // totalOccurences = (lastOccurence-firstOccurence)+1;
+        
+        // 1->total = lastOccurence-firstOccurence+1 
+        // 2->total = UpperBound-LowerBound 
+        
+        int totalOccurences = upperBound(arr, target)-lowerBound(arr, target);
+        return totalOccurences;
+    }
+    
+    // Lower Bound -> nums[i]>=target
+    private int lowerBound(int[] nums, int target){
+        int n = nums.length;
+        int low = 0; 
+        int high = n-1;
+        int result = n; // Default value, start from the last and every time decrease the bound
 
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]>=target){
+                result = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+
+        return result;
+    }
+
+    // Upper Bound -> nums[i]>target
+    private int upperBound(int[] nums, int target){
+        int n = nums.length;
+        int low = 0; 
+        int high = n-1;
+        int result = n; // Default value, start from the last and every time decrease the bound
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]>target){
+                result = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+
+        return result;
+    }
+}
