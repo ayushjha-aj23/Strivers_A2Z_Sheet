12e303c53f14b65c3c821a57c60fe49b67af5d03
Ayush Jha <aj23.workspace@gmail.com>
2025-11-12 12:20:21 +0530

    Update 05_FirstNLastOccurence.java

--- 01_Basics/06_BinarySearch/05_FirstNLastOccurence.java
+++ 01_Basics/06_BinarySearch/05_FirstNLastOccurence.java
@@ -90,5 +90,51 @@ class Solution {
         }
 
         return result;
-    }    
+    }
+
+
+    // Approach 3: Without using Lower and Upper Bound
+    /*
+     public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int result_1 = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result_1 = mid;
+                right = mid - 1; // Continue searching in the left half
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result_1;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int result_2 = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result_2 = mid;
+                left = mid + 1; // Continue searching in the right half
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result_2;
+    }
+    */
+    
 }
