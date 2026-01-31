572866d00d6e5eb326bdf0900bb43b488cdac395
Ayush Jha <aj23.workspace@gmail.com>
2026-01-31 17:20:09 +0530

    Update 20_Pattern.java

--- 01_Basics/02_Patterns[Loops]/20_Pattern.java
+++ 01_Basics/02_Patterns[Loops]/20_Pattern.java
@@ -14,6 +14,9 @@ Pattern 20 - Buttterfly Pattern
 n=5
 */
 
+// Stars | Spaces | Stars
+// For Stars -> stars = i <= n ? i : 2*n - i
+// For Spaces-> spaces = i<n ? spaces-2:spaces+2;
 
 
 public class Main
@@ -21,48 +24,28 @@ public class Main
 	public static void main(String[] args) {
 	    
 	    int n = 5;
-	    int stars_a;
-	    int stars_b;
 	    int spaces = 2*n-2;
 	    
 	    for(int i=1; i<=2*n-1; i++){
-	       
 	         
-	       // Stars 
-	       //int stars = i <= n ? i : 2*n - i;// 1 to 5, then 4 to 1
-	       // OR 
-	        if(i<=n)
-	        stars_a= i;
-	       else 
-	        stars_a = 2*n-i;
+	       // Find Stars 
+	       int stars = i <= n ? i : 2*n - i;// first increase from 1 to 5, then decrease from 4 to 1
 	       
-	       for(int j=1; j<=stars_a; j++){
+			for(int j=1; j<=stars; j++){
 	           System.out.print("*");
 	       }
-	       
 	       
 	       // Spaces 
 	       for(int j=1; j<=spaces;j++){
 	           System.out.print(" ");
 	       }
-	       
 	       spaces = i<n ? spaces-2:spaces+2;
-	       //OR
-	       //if(i<n){
-	       // spaces-=2;}
-	       //else{
-	       // spaces+=2;}
 	       
 	       
 	       // Stars 
-	       //int stars = i <= n ? i : 2*n - i;// 1 to 5, then 4 to 1
-	       // OR 
-	       if(i<=n)
-	        stars_b= i;
-	       else 
-	        stars_b = 2*n-i;
+	       // stars = i <= n ? i : 2*n - i;// 1 to 5, then 4 to 1
 	       
-	       for(int j=1; j<=stars_b; j++){
+	       for(int j=1; j<=stars; j++){
 	           System.out.print("*");
 	       }
 	       
