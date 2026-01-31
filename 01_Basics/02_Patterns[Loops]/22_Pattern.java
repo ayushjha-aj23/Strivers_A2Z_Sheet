b91a6bc2b4c5cf644c132b9a3c632d62eec92065
Ayush Jha <aj23.workspace@gmail.com>
2026-01-31 17:38:48 +0530

    Update 22_Pattern.java

--- 01_Basics/02_Patterns[Loops]/22_Pattern.java
+++ 01_Basics/02_Patterns[Loops]/22_Pattern.java
@@ -7,6 +7,19 @@ Pattern - 22: The Number Pattern
 3 2 1 2 3 
 3 2 2 2 3
 3 3 3 3 3
+
+// Converting the matrix to new matrix by n-value on each element
+// n-value = newElement
+
+0 0 0 0 0 
+0 1 1 1 0
+0 1 2 1 0
+0 1 1 1 0 
+0 0 0 0 0
+
+Now if we are able to generate this matrix then making the initial matrix is easy by doing -> n-newElement
+
+And for making this new matrix we can find the top, left, right, and bottom distance of each element and taking the minimum of them 
 */
 
 
@@ -22,7 +35,8 @@ public class Main
 	            int left = j;	   
 	            int bottom = (2*n-1)-i;
 	            int right = (2*n-1)-j;
-	            
+
+				// Math.min( min(top, bottom), min(left, right) )
 	            System.out.print(n-Math.min(Math.min(top, bottom), Math.min(right, left)));
 	        } 
 	        System.out.println();
