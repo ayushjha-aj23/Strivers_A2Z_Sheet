3b1838584361331fdcce489ba8be412bbb5ab337
Ayush Jha <aj23.workspace@gmail.com>
2026-01-31 17:11:26 +0530

    Update 14_Pattern.java

--- 01_Basics/02_Patterns[Loops]/14_Pattern.java
+++ 01_Basics/02_Patterns[Loops]/14_Pattern.java
@@ -14,6 +14,8 @@ public class Main
 	public static void main(String[] args) {
         int n=5;
         for(int i=1; i<=n; i++){
+
+			// From A to A+i 
             for(char c='A'; c<'A'+i; c++){
                System.out.print(c);
             }
